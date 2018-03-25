a942292b4994fbfc5d64fc59a192785f998f2b92
demel42 <demel42@web.de>
2018-03-25 17:10:42 +0200

    first commit

---
+++ IPSymconMySQL/module.php
@@ -0,0 +1,166 @@
+<?php
+
+class IPSymconMySQL extends IPSModule
+{
+    private $scriptName = 'MySQL';
+
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RegisterPropertyString('server', '');
+        $this->RegisterPropertyInteger('port', '3306');
+        $this->RegisterPropertyString('user', '');
+        $this->RegisterPropertyString('password', '');
+        $this->RegisterPropertyString('database', '');
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        $server = $this->ReadPropertyString('server');
+        $port = $this->ReadPropertyInteger('port');
+        $user = $this->ReadPropertyString('user');
+        $password = $this->ReadPropertyString('password');
+        $database = $this->ReadPropertyString('database');
+
+        if ($server != '' && $port > 0) {
+			$ok = true;
+            if ($server == '') {
+                echo 'no value for property "server"';
+                $ok = false;
+            }
+            if ($user == '') {
+                echo 'no value for property "user"';
+                $ok = false;
+            }
+            if ($password == '') {
+                echo 'no value for property "password"';
+                $ok = false;
+            }
+            if ($database == '') {
+                echo 'no value for property "database"';
+                $ok = false;
+            }
+            $this->SetStatus($ok ? 102 : 201);
+        } else {
+            $this->SetStatus(104);
+        }
+    }
+
+    public function TestConnection()
+    {
+		$rows = $this->ExecuteSimple("select now() as now");
+		if ($rows && isset($rows[0]->now)) {
+			$now = strtotime($rows[0]->now);
+			$tstamp = date("d.m.Y H:i:s", $now);
+			$n = $now - time();
+			if (abs($n) > 1)
+				$msg = ", differ from localtime " . $n . "sec";
+			else
+				$msg = '';
+			echo "current timestamp on database-server is " . $tstamp . $msg;
+			$this->SetStatus(102);
+		} else {
+            $this->SetStatus(201);
+		}
+    }
+
+    public function Open()
+    {
+        $server = $this->ReadPropertyString('server');
+        $port = $this->ReadPropertyInteger('port');
+        $user = $this->ReadPropertyString('user');
+        $password = $this->ReadPropertyString('password');
+        $database = $this->ReadPropertyString('database');
+
+        $this->SendDebug($this->scriptName, "open database " . $database . "@" . $server . ":" . $port . "(user=" . $user . ")", 0);
+
+		$dbHandle = new mysqli($server, $user, $password, $database);
+		if ($dbHandle->connect_errno) {
+			$this->SetBuffer('dbHandle', '');
+			$this->SendDebug($this->scriptName, "can't open database", 0);
+			echo "can't open database " . $database . "@" . $server . ": " . $dbHandle->connect_error . "\n";
+			return false;
+		}
+
+		$this->SendDebug($this->scriptName, "  dbHandle=" . print_r($dbHandle, true), 0);
+		return $dbHandle;
+    }
+
+    public function Close(int $dbHandle)
+    {
+		if ($dbHandle && $dbHandle->close() == false) {
+			echo "unable to close database\n";
+			return false;
+		}
+		return true;
+    }
+
+    public function ExecuteSimple(string $statement)
+    {
+        $server = $this->ReadPropertyString('server');
+        $database = $this->ReadPropertyString('database');
+
+		$dbHandle = $this->Open();
+		if ($dbHandle == false) {
+			return ($dbHandle);
+		}
+
+		$this->SendDebug($this->scriptName, "execute statement \"" . $statement . "\" on " . $database . "@" . $server, 0);
+		$res = $dbHandle->query($statement);
+		if ($res == false) {
+			$this->SendDebug($this->scriptName, "unable to execute statement", 0);
+			echo "unable to execute statement \"" . $statement . "\": " . $dbHandle->error . "\n";
+			return ($res);
+		}
+
+		$this->SendDebug($this->scriptName, "  got ". $res->num_rows . " row", 0);
+
+		if (!isset($res->num_rows)) {
+			return ($res);
+		}
+
+		$rows = '';
+		while ($row = $res->fetch_object()){
+			$rows[] = $row;
+		}
+		$res->close();
+
+		return ($rows);
+    }
+
+    public function Query(int $dbHandle, string $statement)
+    {
+        $server = $this->ReadPropertyString('server');
+        $database = $this->ReadPropertyString('database');
+
+		if ($dbHandle == false) {
+			echo "unable to execute statement \"" . $statement . "\": invalid database-handle\n";
+			return ($dbHandle);
+		}
+
+		$this->SendDebug($this->scriptName, "query \"" . $statement . "\" on " . $database . "@" . $server, 0);
+		$res = $dbHandle->query($statement);
+		if ($res == false) {
+			$this->SendDebug($this->scriptName, "unable to query", 0);
+			echo "unable to execute statement \"" . $statement . "\": " . $dbHandle->error . "\n";
+			return ($res);
+		}
+
+		if (!isset($res->num_rows)) {
+			return ($res);
+		}
+
+		$this->SendDebug($this->scriptName, "  got ". $res->num_rows . " row", 0);
+
+		$rows = '';
+		while ($row = $res->fetch_object()){
+			$rows[] = $row;
+		}
+		$res->close();
+
+		return ($rows);
+    }
+}
---
+++ README.md
@@ -0,0 +1,89 @@
+# MySQL
+
+Modul für IP-Symcon ab Version 4.
+
+## Dokumentation
+
+**Inhaltsverzeichnis**
+
+1. [Funktionsumfang](#1-funktionsumfang)  
+2. [Voraussetzungen](#2-voraussetzungen)  
+3. [Installation](#3-installation)  
+4. [Funktionsreferenz](#4-funktionsreferenz)
+5. [Konfiguration](#5-konfiguartion)  
+6. [Anhang](#6-anhang)  
+
+## 1. Funktionsumfang
+
+Ein vereinfachtes Interface zu einer MySQL-Datenbank für die Basis-Befehle, es wird mit dem mysqli-Interface (http://php.net/manual/de/class.mysqli.php) gearbeitet.
+
+## 2. Voraussetzungen
+
+ - IPS 4.x
+ - Datenbank-Server
+   - Typ: MySQL o┌er MariaDB
+   - Datenbank mit ausreichenden Zugriffsrechten
+
+## 3. Installation
+
+### a. Laden des Moduls
+
+Die IP-Symcon (min Ver. 4.x) Konsole öffnen. Im Objektbaum unter Kerninstanzen die Instanz __*Modules*__ durch einen doppelten Mausklick öffnen.
+
+In der _Modules_ Instanz rechts oben auf den Button __*Hinzufügen*__ drücken.
+ 
+In dem sich öffnenden Fenster folgende URL hinzufügen:
+
+`https://github.com/demel42/IPSymconMySQL.git`
+    
+und mit _OK_ bestätigen.    
+        
+Anschließend erscheint ein Eintrag für das Modul in der Liste der Instanz _Modules_    
+
+### b. Einrichtung in IPS
+
+In IP-Symcon nun _Instanz hinzufügen_ (_CTRL+1_) auswählen unter der Kategorie, unter der man die Instanz hinzufügen will, und Hersteller _(sonstiges)_ und als Gerät _MySQL_ auswählen.
+
+In dem Konfigurationsdialog den Datenbank-Server eintragen (Name oder IP-Adresse sind zulässig), Datenbank und Zugriffdaten
+
+## 4. PHP-Befehlsreferenz
+
+### zentrale Funktion
+
+`(mysqli Object) = MySQL_Open(integer $InstanzID)`
+
+Öffnet die in der Konfiguration angegebene Datenbank und liefert ein _mysqli Object_ zurück. Die Verbindung muss nicht geschlossen werden, aufgrund der _Persistent Connections_ (http://php.net/manual/de/mysqli.persistconns.php) werden offene Verbindungen später wieder verwendet.
+
+`MySQL_Close(integer $InstanzID, object $dbHandle)`
+
+Schliesst die Datenbankverbindung.
+
+`MySQL_ExecuteSimple(integer $InstanzID, string Statement)`
+
+Öffnet die Datenbank und führt des SQL-Statement aus; das Ergebnis ist bei einer Abfrage dier Ergebnismenge als ein Array von Objekten bzw. _false bei einem Fehler (Fehlermeldung dann im Medungsfenster) oder by _update_ / _insert_ / _delete_ wird _true_ oder _false_ geliefert.
+
+`MySQL_Query(integer $InstanzID, object dbHandle, string Statement)`
+
+analog zu _MySQL_ExecuteSimple_, jedoch die der _dbHandle_ übergeben.
+
+## 5. Konfiguration:
+
+### Variablen
+
+| Eigenschaft               | Typ      | Standardwert | Beschreibung |
+| :-----------------------: | :-----:  | :----------: | :----------------------------------------------------------------------------------------------------------: |
+| Server                    | string   |              | Hostname / IP-Adresse des Datenbank-Servers |
+| Port                      | integer  | 3306         | Port, unter dem der Datenbank-Server kommuniziert |
+| Benutzer                  | string   |              | Datenbank-Benutzer |
+| Passwort                  | string   |              | Passwort des Datenbank-Benutzers |
+| Datenbank                 | string   |              | zu benutzende Datenbank |
+
+### Schaltflächen
+
+| Bezeichnung                  | Beschreibung |
+| :--------------------------: | :------------------------------------------------: |
+| Verbindunstest               | Testet die Datenbankverbindung |
+
+## 6. Anhang
+
+GUID: `{C0E06BE4-D9D8-4208-8CDB-93D161A7CA98}` 
